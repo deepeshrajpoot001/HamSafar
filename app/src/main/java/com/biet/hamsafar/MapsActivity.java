343b4b7be1d8f5b611636d4f76eaf352cf6efc31
deepesh <deepeshrajpoot001@gmail.com>
2023-09-07 10:28:57 +0530

    mobile location track done with minute error solved

--- app/src/main/java/com/biet/hamsafar/MapsActivity.java
+++ app/src/main/java/com/biet/hamsafar/MapsActivity.java
@@ -45,6 +45,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
@@ -82,49 +83,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
+        Log.d("ramram","onCreateend");
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case REQUEST_LOCATION_CODE:
-                Log.d("ramram", "hi my name deepesh rajpoot5");
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        if (fusedLocationProviderClient == null) {
-                            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-                        }
-                        mMap.setMyLocationEnabled(true);
-                        checkLocationSettings();
-                        Log.d("ramram", "hi my name deepesh rajpoot4");
-                    }
-                } else {
-                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_LONG).show();
-                }
-        }
 
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        Log.d("ramram","onMapReady");
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
             mMap.setMyLocationEnabled(true);
-           // checkLocationSettings();
         }
 
     }
@@ -132,6 +103,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
+        Log.d("ramram","onLocationChanged");
         lastLocation = location;
         if (currentLocationMarker != null) {
             currentLocationMarker.remove();
@@ -144,17 +116,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
 
         currentLocationMarker = mMap.addMarker(markerOptions);
-
-
-
-
-
-/*
+        /*
         if (fusedLocationProviderClient != null) {
             fusedLocationProviderClient.removeLocationUpdates(this);
         }
-
- */
+          */
 
         Log.d("ramram", markerOptions.getPosition().toString());
         Log.d("ramram", location.getLatitude() + " "+location.getLongitude());
@@ -164,16 +130,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
        //mMap.animateCamera(CameraUpdateFactory.zoomBy(20),5000,null);
         CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(20.0F).build();
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-
-
-
     }
 
 
     private void checkLocationSettings() {
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
                 .addLocationRequest(locationRequest);
+
+        Log.d("ramram","checkLocationSetting");
 
         Task<LocationSettingsResponse> task = LocationServices.getSettingsClient(this)
                 .checkLocationSettings(builder.build());
@@ -183,12 +147,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
                 // All location settings are satisfied. You can start requesting location updates.
                 startLocationUpdates();
+                Log.d("ramram","checkLocationSettingSuccess");
             }
         });
 
         task.addOnFailureListener(this, new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
+                Log.d("ramram","checkLocationSettingfails");
                 int statusCode = ((ResolvableApiException) e).getStatusCode();
                 switch (statusCode) {
                     case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
@@ -211,7 +177,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void startLocationUpdates() {
-        Log.d("ramram","jay shri krishna");
+        Log.d("ramram","startLocationUpdate");
 
         locationRequest = new LocationRequest.Builder(Priority.PRIORITY_BALANCED_POWER_ACCURACY)
                 .setIntervalMillis(1000)
@@ -223,6 +189,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             fusedLocationProviderClient.requestLocationUpdates(locationRequest, this, Looper.myLooper());
             // 2 argument in above function call onLocationChanged()
+            Log.d("ramram","startLocationUpdate2");
         }
 
     }
@@ -230,14 +197,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public boolean checkLocationPermission()
     {
         if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)  != PackageManager.PERMISSION_GRANTED )
-        {
+        {               Log.d("ramram","changeLocationPermission1");
+
 
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.ACCESS_FINE_LOCATION))
             {
+                Log.d("ramram","changeLocationPermission2");
                 ActivityCompat.requestPermissions(this,new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION },REQUEST_LOCATION_CODE);
             }
             else
             {
+                Log.d("ramram","changeLocationPermission3");
                 ActivityCompat.requestPermissions(this,new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION },REQUEST_LOCATION_CODE);
             }
             return false;
@@ -245,7 +215,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         else {
             checkLocationSettings();
-            Log.d("ramram","hi my name deepesh rajpoot3");
+            Log.d("ramram","changeLocationPermission4");
             return true;
         }
     }
@@ -255,12 +225,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (requestCode == REQUEST_CHECK_SETTINGS) {
             if (resultCode == RESULT_OK) {
                 // User enabled location settings
-                startLocationUpdates();
+               // startLocationUpdates();
+                Log.d("ramram", "onActivityResult1");
+                checkLocationSettings();
             } else {
                 // User canceled or declined to enable location settings
+                Log.d("ramram", "onActivityResult2");
                 // Handle this case accordingly.
             }
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_LOCATION_CODE:
+                Log.d("ramram", "onRequestPermissionResult "+requestCode);
+                Log.d("ramram", grantResults.length +"onRequestPermissionResult "+ grantResults[0]);
+                Log.d("ramram", "onRequestPermissionResult "+ PackageManager.PERMISSION_GRANTED);
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        if (fusedLocationProviderClient == null) {
+                            Log.d("ramram", "onRequestPermissionResult1");
+                            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+                        }
+                        mMap.setMyLocationEnabled(true);
+                        checkLocationSettings();
+                        Log.d("ramram", "onRequestPermissionResult2");
+                    }
+                } else {
+                    Log.d("ramram", "onRequestPermissionResult3");
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_LONG).show();
+                }
+        }
+
     }
 
 }
